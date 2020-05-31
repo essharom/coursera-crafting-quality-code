b6cfa2344a8b5cc065ea83db9fe30ef10ee1cd14
Sebestyén Kamp <sebestyen.kamp@turbine.ai>
2020-05-31 19:34:58 +0200

    writing num_buses with test cases

--- assignment_1/a1.py
+++ assignment_1/a1.py
@@ -1,3 +1,5 @@
+import math
+
 def num_buses(n):
     """ (int) -> int
 
@@ -5,11 +7,22 @@ def num_buses(n):
 
     Return the minimum number of buses required to transport n people.
     Each bus can hold 50 people.
-
+    
+    
+    >>> num_buses(0)
+    0
+    >>> num_buses(1)
+    1
+    >>> num_buses(50)
+    1
+    >>> num_buses(51)
+    2
     >>> num_buses(75)
     2
+    
     """
-
+    
+    return math.ceil((n / 50))
 
 def stock_price_summary(price_changes):
     """ (list of number) -> (number, number) tuple
